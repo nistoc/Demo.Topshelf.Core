dbb7c4f911369bc23b0c496351465bac5c0f45a4
Nikita Temnikov <nistoc@gmail.com>
2019-05-22 12:52:54 +0300

    Add initial empty project

---
+++ Demo.API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
